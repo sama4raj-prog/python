1aedc4ff10fc77f1bbd23ddee0abbf2765f4cddc
sama4raj-prog <sama4raj@gmail.com>
2026-04-18 20:44:03 +0600

    activity

---
+++ module 1/buffer activity/activity.py
@@ -0,0 +1,13 @@
+#User input
+
+name = input("What is your name? ")
+age = input("What is your age? ")
+city = input("What city do you live in? ")
+
+#Print format
+print("\n---Name Tag---")
+print("-----------------")
+print("Name: " + name)
+print("Age: " + age)
+print("City: " + city)
+
---
+++ module 1/buffer activity/lesson 2 ACP/activity.py
@@ -0,0 +1,20 @@
+A = "21st January, 2013"
+B = "24th March, 2013"
+C = "17th February, 2013" 
+D = "1st April, 2013"
+E = "5th June, 2013"
+
+#User Input
+name = input("What is your name? ")
+if name == "A":
+    print("Alex was born on " + A)
+elif name == "B":
+    print("Bob was born on " + B)
+elif name == "C":
+    print("Charlie was born on " + C)
+elif name == "D":
+    print("David was born on " + D)
+elif name == "E":
+    print("Emily was born on " + E)
+else:
+    print("Sorry, I don't have information about that person.")
