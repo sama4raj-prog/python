6441aa247633f24a20c0c4444307dfdc727dd607
sama4raj-prog <sama4raj@gmail.com>
2026-04-24 20:21:22 +0600

    activity

---
+++ module 1/lesson 3/activity.py
@@ -0,0 +1,18 @@
+a = 5
+b = 24.3
+c = True
+d = "coding"
+
+print(f"The data type of a is {type(a)}")
+print(f"The data type of b is {type(b)}")
+print(f"The data type of c is {type(c)}")
+print(f"The data type of d is {type(d)}")
+
+#Typecasting
+b = int(b)
+print(f"The value of b is {b}")
+print(f"The data type of b is", {type(b)})
+
+c = str(c)
+print(f"The value of c is {c}")
+print(f"The data type of c is", {type(c)})
