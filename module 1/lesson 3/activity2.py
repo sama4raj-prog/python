2c9b786d016da8d4c4906c88b61caa63397fdb8a
sama4raj-prog <sama4raj@gmail.com>
2026-04-24 20:39:23 +0600

    activity

---
+++ module 1/lesson 3/activity2.py
@@ -0,0 +1,20 @@
+word = input("Enter a word: ")
+
+#Length of the word
+print("the length of the word is", len(word))
+
+#Indexing
+print(word[3])
+
+#Slicing
+print(word[0:4])
+
+#Concatenation
+word2 = "coding"
+print(word + " " + word2)
+
+#converting to uppercase
+print(word.upper())
+
+#Reverse a string
+print(word[::-1])
