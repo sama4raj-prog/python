e9a56a63de307ae7d38807228dd34dd245b4373e
sama4raj-prog <sama4raj@gmail.com>
2026-04-25 21:11:26 +0600

    activity

---
+++ module 1/buffer activity/lesson 4 ACP/activity.py
@@ -0,0 +1,8 @@
+number = float(input("Please enter a number: "))
+
+result = number ** 0.5
+square = number * number
+
+print(f"The square root of {number} is: {result}")
+print(f"The square of {number} is: {square}")
+
