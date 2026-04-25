d39c1f52955f029343cc48ae880b629de114054d
sama4raj-prog <sama4raj@gmail.com>
2026-04-25 20:44:09 +0600

    activity

---
+++ module 1/lesson 4/activity3.py
@@ -0,0 +1,11 @@
+math = 40
+hindi = 50
+english = 60
+science = 70
+
+sum = math+hindi+science+english
+
+percentage = (sum/400)*100
+percentage = round(percentage, 2)
+
+print("The grade percentage is ", percentage)
