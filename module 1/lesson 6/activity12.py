c21ea7c2abe7266ed0439be5d35b79164999659f
sama4raj-prog <sama4raj@gmail.com>
2026-05-02 20:28:47 +0600

    activity

---
+++ module 1/lesson 6/activity12.py
@@ -0,0 +1,11 @@
+a = 10
+b = -10
+
+if a > 0 and b > 0:
+    print("Both of them are positive.")
+
+if a > 0 or b > 0:
+    print("At least one of them is positive.")
+
+if not (a==b):
+    print("Both of the variables are different.")
