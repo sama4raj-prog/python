0567e6aae422cdf52a2382aa3c62fff37c2422d8
sama4raj-prog <sama4raj@gmail.com>
2026-05-26 17:11:37 +0600

    activity

--- test/activity.py
+++ test/activity.py
@@ -6,21 +6,26 @@ hearts = 5
 
 print("Welcome to the Number Guessing Game! You have five hearts. Too cold means the input is greater than the number. Too hot means the input is less than the number. Good luck!")
 
-if hearts > 0 or guess != num:
-    guess = print(input("Guess the number between 1 and 50: "))
-
-if guess > num: 
-    print("Too cold. Try again.")
-    hearts = hearts-1
-    print("You have ", hearts, " hearts left.")
-elif guess < num:  
-    print("Too hot. Try again.")
-    hearts = hearts-1
-    print("You have ", hearts, " hearts left.")
-
-if guess == num:
-    print("Congratulations, You guessed the number!")
+while hearts > 0:
+    print(f"Hearts left: {hearts}")
+    try:
+        guess = int(input("Enter a number between 1-50: "))
+    except ValueError:
+        print("Please enter a valid integer.")
+        exit()
+    if guess == num:
+        print(f"Congratulations! You guessed the secret number {num}!")
+        break
+    else:
+        hearts -= 1
+    if guess > num: 
+        print("Try again. Hint: Too cold.")
+        print("You have ", hearts, " hearts left.")
+    elif guess < num:  
+         print("Try again. Hint: Too hot.")
+         print("You have ", hearts, " hearts left.")
 
 if hearts == 0:
-    print("You lose. The number was ", num)
+    print(f"Game over! The secret number was {num}. Better luck next time!")
+
 
