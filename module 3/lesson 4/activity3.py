fca420f22cae7ac7ec26cee81ccc4d3e0a8b6f44
sama4raj-prog <sama4raj@gmail.com>
2026-06-19 20:36:32 +0600

    activity

---
+++ module 3/lesson 4/activity3.py
@@ -0,0 +1,12 @@
+valid = False
+
+while not valid:
+
+    try: 
+        num = int(input("Enter a number: "))
+        while num%2==0:
+            print("BYE")
+        valid = True
+    
+    except ValueError as ex:
+        print("ValueError: ", ex)
