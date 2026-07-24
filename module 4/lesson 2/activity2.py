0f481324a767da5da9969804f2f7e7deaf7a787b
sama4raj-prog <sama4raj@gmail.com>
2026-07-24 20:31:50 +0600

    activity

--- module 4/lesson 2/activity2.py
+++ module 4/lesson 2/activity2.py
@@ -1,6 +1,12 @@
 my_tuple = (1,2,3,3,2,1)
+my_tuple1 = (1,2,3,4,5,6)
 
 if my_tuple == my_tuple[::-1]:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
+
+if my_tuple1 == my_tuple[::-1]:
     print("Palindrome")
 else:
     print("Not a Palindrome")
