b173527a27bf2980f493d8c7efb3b3023bf29f7d
sama4raj-prog <sama4raj@gmail.com>
2026-07-25 20:42:39 +0600

    activity

---
+++ module 4/lesson 3/activity3.py
@@ -0,0 +1,9 @@
+country_codes = {
+    "Bangladesh": "BD",
+    "India": "IN",
+    "Pakistan": "PK",
+    "Bhutan": "BT"
+}
+
+print(country_codes.get("Bangladesh", "Country not found"))
+print(country_codes.get("Nepal", "Country not found"))
