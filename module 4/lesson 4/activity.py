730c6f71ba5fdfe1646cf84d04e93d52d556492a
sama4raj-prog <sama4raj@gmail.com>
2026-08-01 20:37:03 +0600

    activity

--- module 4/lesson 4/activity.py
+++ module 4/lesson 4/activity.py
@@ -9,3 +9,16 @@ print (f"Basket 2 after adding oranges: {basket2}")
 
 common_fruits = basket1.intersection(basket2)
 print (f"Common fruits in both baskets: {common_fruits}")
+
+import array as arr
+
+fruit_counts = arr.array('i', [5, 3, 2, 4])
+print (fruit_counts)
+
+fruit_counts.insert(0, 1)
+print (fruit_counts)
+fruit_counts.append(6)
+print (fruit_counts)
+
+fruit_counts.reverse()
+print (fruit_counts)
