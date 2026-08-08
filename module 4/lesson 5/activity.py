6835d5852e1179e989dd98dee370ffb0b7841856
sama4raj-prog <sama4raj@gmail.com>
2026-08-08 20:36:16 +0600

    activity

--- module 4/lesson 5/activity.py
+++ module 4/lesson 5/activity.py
@@ -11,3 +11,16 @@ chosen_item = input("What item do you want to buy? ->")
 if chosen_item not in inventory or inventory[chosen_item] == 0:
     print(f"{chosen_item} is out of stock! Stopping the checker.")
     exit()
+    
+prices = [10,5,40,15,20]
+markup = int(input("Enter the markup amount to add to every price: "))
+
+markedup_prices = list(map(lambda p : p + markup,prices))
+print("Marked up prices:", markedup_prices)
+
+item_index = item.index(chosen_item)
+chosen_price = markedup_prices[item_index]
+print(f"The price of {chosen_item} after markup is: ${chosen_price}")
+
+inventory[chosen_item] -= 1
+print(f"{chosen_item} purchased! Remaining stock: {inventory[chosen_item]}")
