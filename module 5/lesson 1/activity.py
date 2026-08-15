d58d7fd83b958470020f763a745f2f175931e186
sama4raj-prog <sama4raj@gmail.com>
2026-08-15 20:14:30 +0600

    activity

---
+++ module 5/lesson 1/activity.py
@@ -0,0 +1,5 @@
+class student:
+    grade = 7
+print("I'm a student of grade", student.grade)
+
+ob = student()
---
+++ test/activitytext.py
@@ -0,0 +1,4 @@
+names = ["Alice", "Bob"]
+scores = [90, 85]
+pairs = list(zip(names, scores))
+print(pairs)
